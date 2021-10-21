952f870ff982156c5b86ff3dcc0adab0970add3c
Mattia3 <mcortinovis9@gmail.com>
2021-10-21 16:12:56 +0200

    Esercizio completato

--- js/script.js
+++ js/script.js
@@ -1,50 +1,50 @@
 //Vado a creare la mia arrey con il nome,la professione e le immagini
 const membriTeam = [
   {//indice 0
-    name : 'Wayne',
-    role : 'Chief Editor',
-    img : 'img/angela-caroll-chief-editor.jpg'
+    name : 'Wayne Barnett',
+    role : 'Founder & CEO',
+    image : 'img/wayne-barnett-founder-ceo.jpg'
   },
   {//indice 1
-    name : 'Walter Gordon',
-    role : 'Office Manager',
-    img : 'img/barbara-ramos-graphic-designer.jpg'
+    name : 'Angela Caroll',
+    role : 'Chief Editor',
+    image  : 'img/angela-caroll-chief-editor.jpg'
   },
   {//indice 2
     name : 'Walter Gordon',
     role : 'Office Manager',
-    img : 'img/walter-gordon-office-manager.jpg'
+    image : 'img/walter-gordon-office-manager.jpg'
   },
   {//indice 3
     name : 'Angela Lopez',
     role : 'Social Media Manager',
-    img : 'img/angela-lopez-social-media-manager.jpg'
+    image  : 'img/angela-lopez-social-media-manager.jpg'
   },
   {//indice 4
     name : 'Scott Estrada',
     role : 'Developer',
-    img : 'img/scott-estrada-developer.jpg'
-  },
+    image : 'img/scott-estrada-developer.jpg'
+  }, 
   {//indice 5
     name : 'Barbara Ramos',
     role : 'Graphic Designer',
-    img : 'img/barbara-ramos-graphic-designer.jpg'
+    image : 'img/barbara-ramos-graphic-designer.jpg'
   },
 
 ]
 console.log(membriTeam) //Vado a stampare l'Array
-
+const teamContainer = document.querySelector(".team-container")
 for (i = 0; i < membriTeam.length; i++){
-  let membro = membriTeam[1];
+  let membro = membriTeam[i];
 
   for (let key in membro){
 
   }
 
-  const teamContainer = document.querySelector(".team-container")
+  
   let allCard = `<div class="team-card">
                   <div class="card-image">
-                     <img src="${membro.img}" alt="Wayne Barnett" />
+                     <img src="${membro.image}" alt="Wayne Barnett" />
                   </div>
                   <div class="card-text">
                     <h3>${membro.name}</h3>
@@ -53,7 +53,34 @@ for (i = 0; i < membriTeam.length; i++){
                 </div>`;
 
   teamContainer.innerHTML += allCard;
-  console.log(membro.img)
+  console.log(membro.image)
   console.log(membro.name)
   console.log(membro.role)
 }
+
+const buttonAdd = document.getElementById("addMemberButton")
+let nameInput = document.getElementById("name")
+let roleInput = document.getElementById("role")
+let imgInput = document.getElementById("image")
+
+buttonAdd.addEventListener("click", function(){
+  nameInput.value
+  roleInput.value
+  imgInput.value
+  
+  let inputStart = `<div class="team-card">
+  <div class="card-image">
+     <img src="${imgInput.value}" alt="${imgInput.value}" />
+  </div>
+  <div class="card-text">
+    <h3>${nameInput.value}</h3>
+    <p>${roleInput.value}</p>
+  </div>
+</div>`;
+
+teamContainer.innerHTML += inputStart;
+console.log(nameInput)
+console.log(imgInput)
+console.log(roleInput)
+})
+
